be6713ed2ff936fdd3fdbbe55cd3187ade9456ae
Shushant-K-Singh <shushantkumar860@gmail.com>
2026-01-10 15:12:05 +0530

    Conditions and Operators

---
+++ 2OperatorsAndConditionalStatements/operators.js
@@ -0,0 +1,16 @@
+// Operators 
+//1. Aritmatic Operators  +,-,*,/,%,**,++,--
+let a = 12;
+let b = "21";
+console.log(typeof b);
+
+console.log(a+b); //1221
+console.log(a-b); //-9
+console.log(a*b); //252
+console.log(++b); //22
+console.log(b+1); //23
+console.log(typeof string(b));
+
+//2. Assignment Operators =, +=, -=, *=, /=
+
+//3. comparison Operator

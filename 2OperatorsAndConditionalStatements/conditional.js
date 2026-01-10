8bd5a1b21e2415cb339d36bc03e56c092fbed8f8
Shushant-K-Singh <shushantkumar860@gmail.com>
2026-01-10 15:21:40 +0530

    all updates till now

---
+++ 2OperatorsAndConditionalStatements/conditional.js
@@ -0,0 +1,48 @@
+//CONDITIONAL STATEMENTS
+// if statement
+let marks = 88;
+if (marks>=90){
+    console.log("Grade A+");
+}
+else if(marks>=80){
+    console.log("Grade A");
+}
+else{
+    console.log("Need Improvement");
+}
+
+
+// Switch Statement
+let day = 3;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+
+
+
+
+
+
